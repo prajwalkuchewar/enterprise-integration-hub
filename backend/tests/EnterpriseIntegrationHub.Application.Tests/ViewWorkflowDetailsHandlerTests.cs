6030984b8e842540c23b0ada0f8dd03604551487
Prajwal Kuchewar <prajwalkuchewar3@gmail.com>
2026-08-02 17:32:25 +0530

    chore: reorganize using directives in ViewWorkflowDetailsHandlerTests

--- backend/tests/EnterpriseIntegrationHub.Application.Tests/ViewWorkflowDetailsHandlerTests.cs
+++ backend/tests/EnterpriseIntegrationHub.Application.Tests/ViewWorkflowDetailsHandlerTests.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using EnterpriseIntegrationHub.Application.Features.Workflows.ViewDetails;
 using EnterpriseIntegrationHub.Application.Interfaces;
 using EnterpriseIntegrationHub.Domain.Entities;
 using FluentAssertions;
 using Moq;
+using Xunit;
 
 namespace EnterpriseIntegrationHub.Application.Tests;
 
